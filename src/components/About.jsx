2e4a4c27324e989d933a5629af22ab3c1cd013a5
Rahat-Hridoy <rahathridoyd2x@gmail.com>
2025-09-19 08:49:39 +0600

    Section: Hero, About done

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,60 @@
 import React from "react";
+import { LuMouse } from "react-icons/lu";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <section className="bg-second-bg relative ">
+      <div className="absolute inset-0 bg-[url('/image/about_bg.png')] bg-no-repeat bg-cover bg-center opacity-10 "></div>
+      <div className="container mx-auto py-[128px] relative z-10">
+        <div className="wrapper">
+          <div className="flex justify-center items-center text-brand-1">
+            <LuMouse className="w-8 h-11 " />
+          </div>
+          <div className="flex justify-between items-center pt-[134px] ">
+            {/* Left side-about description */}
+            <div className="flex flex-col gap-16  ">
+              {/* Head */}
+              <h1 className="max-w-[367px] font-primary font-normal text-primary text-[64px] leading-[72px] px-[40px] py-[16px] border-4 border-brand-1 rounded-tl-[50px] rounded-br-[50px] bg-primary-bg  ">
+                About Me
+              </h1>
+              {/* Description */}
+              <div className="px-[40px] py-[24px] bg-primary-bg rounded-3xl">
+                <p className="tag-style"> {"<p>"} </p>
+                <h4 className="font-second font-medium text-brand-1 text-[32px] leading-[42px] pt-[16px] ">
+                  Hello !
+                </h4>
+                <p className="pera-text text-[16px] leading-[20px] text-justify max-w-[880px] py-[16px] ">
+                  My name is Rahat and I specialize in web developement that
+                  utilizes
+                  <span className="text-brand-1"> HTML</span> ,
+                  <span className="text-brand-1"> CSS</span>{" "}
+                  <span className="text-brand-1"> JS</span>{" "}
+                  <span className="text-brand-1"> React</span> etc. <br />
+                  <br /> I am a highly motivated individual and eternal optimist
+                  dedicated to writing clear, concise, robust code that works.
+                  Striving to never stop learning and improving. <br />
+                  <br /> When I'm not coding, I am writing bolgs, reading, or
+                  picking up some new hands-on art project like photography.{" "}
+                  <br />
+                  <br /> I like to have my perspective and belief systems
+                  challenged so that I see the world through new eyes.
+                </p>
+                <p className="tag-style"> {"</p>"} </p>
+              </div>
+            </div>
+            {/* right side - image */}
+            <div>
+              <img
+                src="/image/about_me.jpg"
+                alt="image"
+                className="w-[462px] h-[556px] rounded-3xl object-cover object-center overflow-hidden  "
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default About;
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -32,76 +32,135 @@ const Hero = () => {
 
   const skills = ["HTML", "CSS", "JS", "React"];
 
+  const exp = [
+    {
+      digit: 4,
+      title: "Programming Language",
+    },
+    {
+      digit: 6,
+      title: "Development Tools",
+    },
+    {
+      digit: 8,
+      title: "Year of experiance",
+    },
+  ];
+
   return (
-    <section className="bg-primary-bg h-screen">
+    <section className="bg-primary-bg ">
       <div className="container mx-auto">
-        <div className="wrapper">
+        <div className="wrapper pb-[128px] ">
           <div>
             <h1 className="font-primary font-normal text-brand-2 text-[117px] text-center leading-[134px] py-[64px]">
               Developer
             </h1>
           </div>
-          {/* left-side */}
-          <div className="w-[320px] h-[520px] border-4 border-primary  px-[22px] py-[36px] relative">
-            <div className="before:content-[' '] "></div>
-            <div>
-              {/* profile */}
+          <div className=" flex justify-between items-center">
+            {/* left-side */}
+            <div className="w-[320px] h-[520px] border-4 border-primary  px-[22px] py-[36px] rounded-tl-[160px] rounded-br-[160px] relative">
               <div>
-                <div className="flex flex-col justify-center items-center gap-[16px] ">
-                  <img
-                    src="/public/image/rahatHridoy.jpeg"
-                    alt="image"
-                    className="w-[90px] h-[90px] rounded-full border-4 border-brand-1 "
-                  />
+                {/* profile */}
+                <div>
+                  <div className="flex flex-col justify-center items-center gap-[16px] ">
+                    <img
+                      src="/public/image/rahatHridoy.jpeg"
+                      alt="image"
+                      className="w-[90px] h-[90px] rounded-full border-4 border-brand-1 "
+                    />
+                  </div>
+                  <h3 className="font-second font-medium text-primary text-[32px] leading-[42px] text-center">
+                    Rahat
+                  </h3>
+                  <h5 className="font-second font-normal text-primary text-sm leading-[18px] text-center">
+                    Full-stack Developer{" "}
+                  </h5>
                 </div>
-                <h3 className="font-second font-medium text-primary text-[32px] leading-[42px] text-center">
-                  Rahat
-                </h3>
-                <h5 className="font-second font-normal text-primary text-sm leading-[18px] text-center">
-                  Full-stack Developer{" "}
-                </h5>
-              </div>
-              {/* information */}
-              <div>
-                <ul className="py-[32px] flex flex-col justify-center items-start gap-y-[16px] ">
-                  {info.map((item) => (
-                    <li
-                      key={item.id}
-                      className="flex items-center justify-start gap-4 "
-                    >
-                      <span className="text-brand-1 w-[14px] h-[14px] ">
-                        {item.icon}
-                      </span>
+                {/* information */}
+                <div>
+                  <ul className="py-[32px] flex flex-col justify-center items-start gap-y-[16px] ">
+                    {info.map((item) => (
+                      <li
+                        key={item.id}
+                        className="flex items-center justify-start gap-4 "
+                      >
+                        <span className="text-brand-1 w-[14px] h-[14px] ">
+                          {item.icon}
+                        </span>
 
-                      <span className="pera-text text-sm leading-[18px]  ">
-                        {item.text}
-                      </span>
-                    </li>
+                        <span className="pera-text text-sm leading-[18px]  ">
+                          {item.text}
+                        </span>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+                {/* skills */}
+                <div className="flex items-center justify-start gap-4 pb-8  ">
+                  {skills.map((skill) => (
+                    <span
+                      key={skill}
+                      className="max-w-[60px] pera-text text-sm leading-[18px] px-2 bg-brand-1 rounded-full "
+                    >
+                      {skill}
+                    </span>
                   ))}
-                </ul>
-              </div>
-              {/* skills */}
-              <div className="flex items-center justify-start gap-4 pb-8  ">
-                {skills.map((skill) => (
-                  <span
-                    key={skill}
-                    className="max-w-[60px] pera-text text-sm leading-[18px] px-2 bg-brand-1 rounded-full "
-                  >
-                    {skill}
+                </div>
+                {/* CV button */}
+                <button className="bg-primary rounded-full flex items-center justify-center gap-x-4 px-8 py-4 duration-300 hover:cursor-pointer hover:bg-primary/80 active:bg-primary active:scale-95 ">
+                  <span className="font-primary font-normal text-[20px] text-primary-bg leading-[24px] ">
+                    Download CV
                   </span>
-                ))}
+                  <LuDownload className="text-primary-bg w-6 h-6 " />
+                </button>
               </div>
-              {/* CV button */}
-              <button className="bg-primary rounded-full flex items-center justify-center gap-x-4 px-8 py-4 duration-300 hover:cursor-pointer hover:bg-primary/80 active:bg-primary active:scale-95 ">
-                <span className="font-primary font-normal text-[20px] text-primary-bg leading-[24px] ">
-                  Download CV
-                </span>
-                <LuDownload className="text-primary-bg w-6 h-6 " />
-              </button>
+            </div>
+
+            {/* Middle-side */}
+            <div className="flex flex-col  gap-y-[32px] ">
+              <div>
+                <p className="tag-style"> {"<h1>"} </p>
+                <h1 className="intro-text pl-6">
+                  {" "}
+                  Hey <br /> I'm <span className="text-brand-1">
+                    Rahat
+                  </span>{" "}
+                  <br />
+                  Full-Stack Devoloper{" "}
+                </h1>
+                <p className="tag-style"> {"</h1>"} </p>
+              </div>
+              <div>
+                <p className="tag-style"> {"<p>"} </p>
+                <p className="max-w-[650px] pera-text text-[16px] leading-[20px] pl-6 ">
+                  I help business grow by crafting amazing web experiences. If
+                  you’re looking for a developer that likes to get stuff done,
+                </p>
+                <p className="tag-style"> {"</p>"} </p>
+              </div>
+              <div>
+                <button className="flex items-center justify-start gap-x-[16px] font-second font-medium text-brand-1 text-[32px] leading-[42px] pl-6 hover:cursor-pointer hover:underline">
+                  <span>Let’s Talk</span> <MdOutlineEmail />
+                </button>
+              </div>
+            </div>
+            {/* right side-card */}
+            <div className="bg-second-bg rounded-[80px] px-[32px] py-[48px]">
+              {exp.map((item) => (
+                <div
+                  key={item}
+                  className=" flex justify-center items-center gap-x-4 py-[16px]  "
+                >
+                  <p className="font-second font-medium text-brand-1 text-[48px] leading-[62px] ">
+                    {item.digit}
+                  </p>
+                  <p className="pera-text text-[16px] leading-[20px] max-w-[106px] ">
+                    {item.title}
+                  </p>
+                </div>
+              ))}
             </div>
           </div>
-
-          {/* Right-side */}
         </div>
       </div>
     </section>
