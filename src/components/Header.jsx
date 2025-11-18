69dbafb4e7ceb2e25f8eefaf195428d16c8912ed
MD RAHATUL ISLAM <rahathridoyd2x@gmail.com>
2025-11-18 13:40:53 +0600

    Active : see more button on project section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,6 +10,7 @@ const Header = () => {
           className="wrapper flex items-center justify-between gap-7 min-h-[60px] md:min-h-[90px] px-[10px] md:px-[28px] 
           bg-black/30 backdrop-blur-lg  rounded-2xl shadow-2xl"
         >
+          {/* logo */}
           <div>
             <a
               href="/"
@@ -19,7 +20,8 @@ const Header = () => {
               <span className="text-brand-1 "> {"<Rahat/> "} </span>
             </a>
           </div>
-          <div className="flex items-center justify-between">
+          <div className="flex items-center justify-between gap-14">
+            {/* nav-list */}
             <ul className="flex items-center justify-between gap-x-[16px] md:gap-x-[32px]">
               <li className={`nav-list text-primary hover:cursor-pointer `}>
                 <a href="#home">Home</a>
@@ -28,14 +30,16 @@ const Header = () => {
                 <a href="#project">Projects</a>
               </li>
             </ul>
-            <div className="flex items-center bg-transparent lg:bg-primary rounded-full py-[6px] px-0 md:px-[16px] mx-[32px] 2xl:mx-[64px] ">
+            {/* search */}
+            {/* <div className="flex items-center bg-transparent lg:bg-primary rounded-full py-[6px] px-0 md:px-[16px] mx-[32px] 2xl:mx-[64px] ">
               <input
                 type="text"
                 name="search"
                 className="outline-none hidden lg:block  "
               />
               <CiSearch className="w-[20px] h-[20px] text-primary lg:text-primary-bg hover:cursor-pointer " />
-            </div>
+            </div> */}
+            {/* social icon */}
             <ul className="hidden md:flex items-center justify-between gap-x-[32px]">
               <li className="nav-menu-social">
                 <a
--- src/components/project/Project.jsx
+++ src/components/project/Project.jsx
@@ -1,7 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import SectionHead from "../common/SectionHead";
 import ProjectCard from "./ProjectCard";
-import Btn from "../common/Btn";
 
 const Project = () => {
   const cardContent = [
@@ -32,7 +31,74 @@ const Project = () => {
       github: "https://github.com/Rahat-Hridoy/lesson",
       liveurl: "https://lesson-jet.vercel.app/",
     },
+    {
+      id: 4,
+      img: "/image/LMS_Website.png",
+      title: "LMS Website",
+      text: " Built my first project with React.js and Tailwind CSS — a simple LMS website. Learned component-based development, responsive design, and state management.",
+      projectTech: ["React", "Tailwind CSS", "CSS"],
+      github: "https://github.com/Rahat-Hridoy/lesson",
+      liveurl: "https://lesson-jet.vercel.app/",
+    },
+    {
+      id: 5,
+      img: "/image/LMS_Website.png",
+      title: "LMS Website",
+      text: " Built my first project with React.js and Tailwind CSS — a simple LMS website. Learned component-based development, responsive design, and state management.",
+      projectTech: ["React", "Tailwind CSS", "CSS"],
+      github: "https://github.com/Rahat-Hridoy/lesson",
+      liveurl: "https://lesson-jet.vercel.app/",
+    },
+    {
+      id: 6,
+      img: "/image/LMS_Website.png",
+      title: "LMS Website",
+      text: " Built my first project with React.js and Tailwind CSS — a simple LMS website. Learned component-based development, responsive design, and state management.",
+      projectTech: ["React", "Tailwind CSS", "CSS"],
+      github: "https://github.com/Rahat-Hridoy/lesson",
+      liveurl: "https://lesson-jet.vercel.app/",
+    },
+    {
+      id: 7,
+      img: "/image/LMS_Website.png",
+      title: "LMS Website",
+      text: " Built my first project with React.js and Tailwind CSS — a simple LMS website. Learned component-based development, responsive design, and state management.",
+      projectTech: ["React", "Tailwind CSS", "CSS"],
+      github: "https://github.com/Rahat-Hridoy/lesson",
+      liveurl: "https://lesson-jet.vercel.app/",
+    },
+    {
+      id: 8,
+      img: "/image/LMS_Website.png",
+      title: "LMS Website",
+      text: " Built my first project with React.js and Tailwind CSS — a simple LMS website. Learned component-based development, responsive design, and state management.",
+      projectTech: ["React", "Tailwind CSS", "CSS"],
+      github: "https://github.com/Rahat-Hridoy/lesson",
+      liveurl: "https://lesson-jet.vercel.app/",
+    },
+    {
+      id: 9,
+      img: "/image/LMS_Website.png",
+      title: "LMS Website",
+      text: " Built my first project with React.js and Tailwind CSS — a simple LMS website. Learned component-based development, responsive design, and state management.",
+      projectTech: ["React", "Tailwind CSS", "CSS"],
+      github: "https://github.com/Rahat-Hridoy/lesson",
+      liveurl: "https://lesson-jet.vercel.app/",
+    },
   ];
+
+  const [visibleCount, setVisibleCount] = useState(3);
+
+  const handleShowMore = () => {
+    if (allShown) {
+      setVisibleCount((prev) => prev - prev + 3);
+    } else {
+      setVisibleCount((prev) => prev + 3);
+    }
+  };
+
+  const allShown = visibleCount >= cardContent.length;
+
   return (
     <section className="section-padding" id="project">
       <div className="container mx-auto">
@@ -42,13 +108,24 @@ const Project = () => {
             text={"I had the pleasure of working with these awesome projects"}
           />
         </div>
-        <div className="grid grid-cols-1  lg:grid-cols-3 justify-items-center gap-6   ">
-          {cardContent.map((item) => (
-            <ProjectCard cardContent={item} />
+
+        {/* CARDs */}
+        <div className="grid grid-cols-1 lg:grid-cols-3 justify-items-center gap-6">
+          {cardContent.slice(0, visibleCount).map((item) => (
+            <ProjectCard key={item.id} cardContent={item} />
           ))}
         </div>
-        <div>
-          <Btn text={"See More"} color={"bg-brand-1 mt-[64px] mx-auto "} />
+
+        {/* SHOW MORE BUTTON */}
+        <div className="mt-12">
+          <button
+            onClick={handleShowMore}
+            className={`mx-auto bg-brand-1 rounded-full flex items-center justify-center gap-x-4 px-8 py-4 duration-300 hover:cursor-pointer active:scale-95`}
+          >
+            <span className="font-primary font-medium text-[20px] text-primary-bg leading-[24px] ">
+              {allShown ? "Show Less" : "See More"}
+            </span>
+          </button>
         </div>
       </div>
     </section>
